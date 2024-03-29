047074d232bc8d9fd6e715d73d97802aea000f68
root <root@DESKTOP-8VTPROF>
2024-03-29 03:53:48 +0200

    swap

---
+++ swap.c
@@ -0,0 +1,24 @@
+#include "monty.h"
+/**
+ * swap - swaps the top two elements of the stack
+ * @stack : pointer of the pointer to the top.
+ * @line_number : command line number.
+ *
+ * Return: Void.
+ */
+void swap(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp;
+
+	if ((*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	tmp = (*stack)->next;
+	(*stack)->next = tmp->next;
+	(*stack)->prev = tmp;
+	tmp->next = *stack;
+	tmp->prev = NULL;
+	*stack = tmp;
+}
