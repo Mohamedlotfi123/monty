9aca13e3240b13edf618b7a2c2b6aefa99edf913
root <root@DESKTOP-8VTPROF>
2024-03-30 22:42:45 +0200

    fix 4

--- add.c
+++ add.c
@@ -11,7 +11,7 @@ void add(stack_t **stack, unsigned int line_number)
 	int sum = 0;
 	stack_t *tmp;
 
-	if ((*stack)->next == NULL)
+	if (*stack == NULL || (*stack)->next == NULL)
 	{
 		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
