c054b7789ac7ed60dd9dd87881bf71312655489b
root <root@DESKTOP-8VTPROF>
2024-03-31 02:43:30 +0200

    pchar

--- pchar.c
+++ pchar.c
@@ -20,7 +20,7 @@ void pchar(stack_t **stack, unsigned int line_number)
 		printf("%c\n", value);
 	else
 	{
-		fprintf(stderr, "L%d: can't pchar, value out of range", line_number);
+		fprintf(stderr, "L%d: can't pchar, value out of range\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 }
